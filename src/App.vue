61a3463ea7e34a2ee47ed76e0e443b54d5d0f789
Shahadat Hosain <shahadat80@gmail.com>
2023-08-14 21:23:11 +0600

    first

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Carousel from './components/Carousel.vue'
+import PieChart from './components/PieChart.vue'
+</script>
+
+<template>
+  <Carousel />
+  <PieChart />
+</template>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import Chart from 'chart.js/auto';
+
+const chartRef = ref(null);
+let chartInstance = null;
+
+onMounted(() => {
+    // Fetch data from a sample data source or manually create data for the chart
+    const chartData = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+        {
+        data: [10, 20, 30],
+        backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+        },
+    ],
+    };
+
+    // Create the pie chart
+    //const ctx = chartRef.value.getContext('2d');
+    const ctx = document.getElementById('myChart');
+    chartInstance = new Chart(ctx, {
+        type: 'pie',
+        data: chartData,
+    });
+});
+
+onBeforeUnmount(() => {
+    // Clean up the chart instance
+    if (chartInstance) {
+    chartInstance.destroy();
+    chartInstance = null;
+    }
+});
+
+</script>
+
+<style>
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
